0d4e057a18abcf7b2aa433786192bedd98bb28f6
Matthew Witek <matt@mwitekdesign.com>
2015-11-08 22:44:50 -0500

    Add position to projects table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109032812) do
+ActiveRecord::Schema.define(version: 20151109034352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20151109032812) do
     t.string   "name"
     t.string   "description"
     t.string   "url",         limit: 2083
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
+    t.integer  "position",                 default: 0
   end
 
 end
